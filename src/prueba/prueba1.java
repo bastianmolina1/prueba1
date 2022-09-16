9928d0b8e9beaee81d67a77d38bde27ddbb0d1b6
Bastian <Bastian@Bastian-PC>
2022-09-15 23:35:55 -0300

    first commit

---
+++ src/prueba/prueba1.java
@@ -0,0 +1,16 @@
+
+package prueba;
+
+import prueba.DTO.usuario;
+
+public class prueba1 {
+
+    public static void prueba1(String[] args) {
+        
+        System.out.println("Crear Usuario");
+        usuario usuarioN1 = new usuario();
+
+
+}
+
+
